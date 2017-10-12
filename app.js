2f69c539cf6832994d85cdc2c73aa6a8841e8eab
Ben Lacy <ben.lacy@outlook.com>
2017-10-12 14:11:21 -0500

    particle background

--- app.js
+++ app.js
@@ -1,108 +1,61 @@
-// PARTICLE JS
+
+
+
+// PARTICLE JS BACKGROUND
 var options = {
     "particles":{
         "number":{
-            "value":99,
+            "value":100,
             "density":{
                 "enable":true,
-                "value_area":552.4033491425909
+                "value_area":400
             }
         },
         "color":{
             "value":"#ffffff"
         },
         "shape":{
-            "type":"circle",
-            "stroke":{
-                "width":0,
-                "color":"#000000"
-            },
-            "polygon":{
-                "nb_sides":3
-            },
-            "image":{
-                "src":"img/github.svg",
-                "width":70,
-                "height":100
-            }
+            "type":"circle"
         },
         "opacity":{
             "value":1,
-            "random":true,
-            "anim":{
-                "enable":false,
-                "speed":1,
-                "opacity_min":0.1,
-                "sync":false
-            }
+            "random":true
         },
         "size":{
             "value":2,
             "random":true,
             "anim":{
                 "enable":false,
-                "speed":40,
+                "speed":20,
                 "size_min":0.1,
                 "sync":false
             }
         },
         "line_linked":{
             "enable":false,
-            "distance":150,
+            "distance":50,
             "color":"#ffffff",
             "opacity":0.4,
             "width":1
         },
         "move":{
             "enable":true,
-            "speed":1.5782952832645452,
+            "speed":1,
             "direction":"none",
             "random":true,
             "straight":false,
             "out_mode":"out",
-            "bounce":false,
-            "attract":{
-                "enable":false,
-                "rotateX":600,
-                "rotateY":1200
-            }
+            "bounce":false
         }
     },
     "interactivity":{
         "detect_on":"canvas",
         "events":{
             "onhover":{
-                "enable":false,
-                "mode":"repulse"
+                "enable":false
             },"onclick":{
-                "enable":true,
-                "mode":"repulse"
+                "enable":false
             },"resize":true
-        },
-        "modes":{
-            "grab":{
-                "distance":400,
-                "line_linked":{
-                    "opacity":1
-                }
-            },
-            "bubble":{
-                "distance":400,
-                "size":40,
-                "duration":2,
-                "opacity":8,
-                "speed":3
-            },
-            "repulse":{
-                "distance":200,
-                "duration":0.4
-            },
-            "push":{
-                "particles_nb":4
-            },
-            "remove":{
-                "particles_nb":2
-            }
         }
     },
     "retina_detect":false
